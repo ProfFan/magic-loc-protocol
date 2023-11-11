ffae88746001e4b764a53cf7466c8a8736e8b72d
Fan Jiang <i@fanjiang.me>
2023-11-10 19:52:31 -0500

    packet structure

--- src/packet.rs
+++ src/packet.rs
@@ -1,6 +1,36 @@
+use bilge::prelude::*;
 
 // A poll packet
-#[derive(Debug, Clone, Copy)]
+#[bitsize(48)]
+#[derive(FromBits, DebugBits, PartialEq)]
 pub struct PollPacket {
-    pub tx_timestamp: u64,
+    pub packet_type: PacketType,
+    pub resv: u4,
+    pub tx_timestamp: u40,
+}
+
+#[bitsize(4)]
+#[derive(FromBits, Debug, PartialEq)]
+enum PacketType {
+    Poll = 0,
+    Response = 1,
+    Final = 2,
+    #[fallback]
+    Reserved,
+}
+
+// Tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_poll_packet() {
+        let poll_packet =
+            PollPacket::new(PacketType::Poll, u4::new(0), u40::new(0x12356789).into());
+
+        let poll_packet_bytes = poll_packet.value.to_le_bytes();
+
+        assert_eq!(poll_packet_bytes, [0x00, 0x89, 0x67, 0x35, 0x12, 0x00]);
+    }
 }
